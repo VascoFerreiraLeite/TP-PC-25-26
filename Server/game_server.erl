def3036499bed4644fdca4b1d4005ddd8e2f98f7
VascoFerreiraLeite <165024707+VascoFerreiraLeite@users.noreply.github.com>
2026-04-09 17:40:43 +0100

    Basic TCP connection between server and client

---
+++ Server/game_server.erl
@@ -0,0 +1,31 @@
+-module(game_server).
+-export([start/1, accept_loop/1, client_handler/1]).
+
+start(Port) ->
+    {ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 2}, {reuseaddr, true}, {active, false}]),
+    io:format("Server started on port ~p~n", [Port]),
+    accept_loop(LSock).
+
+accept_loop(LSock) ->
+
+    {ok, ClientSocket} = gen_tcp:accept(LSock),
+    spawn(fun() -> client_handler(ClientSocket) end),
+    accept_loop(LSock).
+
+client_handler(Socket) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, <<ID:8, Payload/binary>>} ->
+            handle_packet(ID, Payload, Socket),
+            client_handler(Socket); 
+        {error, closed} ->
+            io:format("Player disconnected.~n");
+        {error, Reason} ->
+            io:format("Error: ~p~n", [Reason])
+    end.
+
+handle_packet(1, Payload, Socket) ->
+    io:format("Player joined with data: ~p~n", [Payload]),
+    gen_tcp:send(Socket, <<101:8, 100:16, 100:16>>);
+
+handle_packet(ID, _Payload, _Socket) ->
+    io:format("Received unknown packet ID: ~p~n", [ID]).
