13ff7d996f78f034b0081e366de8904c46fd7e0a
VascoFerreiraLeite <165024707+VascoFerreiraLeite@users.noreply.github.com>
2026-04-10 18:17:56 +0100

    cenas

--- Client/src/GameApp.java
+++ Client/src/GameApp.java
@@ -2,8 +2,7 @@ public class GameApp {
     public static void main(String[] args) {
         GameClient client = new GameClient();
         client.connect("localhost", 8080);
-
-        // Send Login Packet (ID: 1) with username "Player1"
+        
         String username = "Player1";
         client.sendPacket(1, username.getBytes());
     }
--- Client/src/GameClient.java
+++ Client/src/GameClient.java
@@ -12,7 +12,6 @@ public class GameClient {
             out = new DataOutputStream(socket.getOutputStream());
             in = new DataInputStream(socket.getInputStream());
 
-            // Start a separate thread to listen for server updates
             new Thread(this::listenFromServer).start();
 
             System.out.println("Connected to Erlang server!");
@@ -21,18 +20,13 @@ public class GameClient {
         }
     }
 
-    // This method corresponds to Erlang's {packet, 2}
     public void sendPacket(int id, byte[] payload) {
         try {
-            // Write the total length of the message (ID + Payload)
-            // {packet, 2} expects a 16-bit unsigned integer prefix
             int totalLength = 1 + payload.length;
             out.writeShort(totalLength);
 
-            // Write the Packet ID
             out.writeByte(id);
 
-            // Write the data
             out.write(payload);
             out.flush();
         } catch (IOException e) {
@@ -43,24 +37,19 @@ public class GameClient {
     private void listenFromServer() {
         try {
             while (true) {
-                // 1. Read the 2-byte length header (handled by DataInputStream)
                 int length = in.readUnsignedShort();
 
-                // 2. Read the Packet ID
                 int packetId = in.readByte();
 
-                // 3. Handle based on ID
                 switch (packetId) {
-                    case 101: // Login Success
+                    case 101:
                         int x = in.readShort();
                         int y = in.readShort();
                         System.out.println("Spawned at: " + x + ", " + y);
-                        // Here you would trigger a method to update your Java GUI
                         break;
 
                     default:
                         System.out.println("Unknown packet ID: " + packetId);
-                        // Skip the rest of the payload if unknown
                         in.skipBytes(length - 1);
                         break;
                 }
